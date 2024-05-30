ccc43dc224edf6722992bff87cdab5127ab98f9c
Tõnu Tarve <tonu.tarve99@hotmail.com>
2024-05-30 05:47:01 +0300

    git init, kursus kuni 11 (va)

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,22 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('posts');
 });
+
+Route::get('posts/{post}', function ($slug) {
+
+    if (!file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html")) {
+        return redirect('/');
+    }
+
+    $post = cache()->remember("posts.{$slug}", 3600, function () use ($path) {
+        var_dump('file_get_contents');
+        return file_get_contents($path);
+    });
+
+    return view('post', [ 'post' => $post ]);
+
+})->where('post', '[A-z_\-]+');
+
+
